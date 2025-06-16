220d9dcae86bccc00fbbc13d9dba1a1da937326a
jdude08221 <jacob.p.nordland@outlook.com>
2025-06-16 03:24:14 -0500

    prod updates

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import asyncio
 from dustkid_api import fetch_all_levels_for_area
 import datetime
 import pprint
+import os
 
 app = Flask(__name__)
 
@@ -220,4 +221,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
